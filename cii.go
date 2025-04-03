73c79e6c02925daf2f96ada97279a14649bd8d00
Sam Lown <sam.lown@invopop.com>
2025-04-03 13:33:39 +0000

    Perform check for appropriate context addons

--- cii.go
+++ cii.go
@@ -5,7 +5,12 @@ import (
 	"fmt"
 
 	"github.com/invopop/gobl"
+	"github.com/invopop/gobl/addons/de/xrechnung"
+	"github.com/invopop/gobl/addons/de/zugferd"
+	"github.com/invopop/gobl/addons/eu/en16931"
+	"github.com/invopop/gobl/addons/fr/facturx"
 	"github.com/invopop/gobl/bill"
+	"github.com/invopop/gobl/cbc"
 )
 
 // CII namespaces
@@ -27,32 +32,40 @@ const (
 type Context struct {
 	GuidelineID string
 	BusinessID  string
+	Addons      []cbc.Key
 }
 
 // ContextEN16931 is used for EN 16931 documents, and is the default.
 var ContextEN16931 = Context{
 	GuidelineID: "urn:cen.eu:en16931:2017",
+	Addons:      []cbc.Key{en16931.V2017},
 }
 
 // ContextPeppolV3 for Peppol Billing V3.0 context.
 var ContextPeppolV3 = Context{
 	GuidelineID: "urn:cen.eu:en16931:2017#compliant#urn:fdc:peppol.eu:2017:poacc:billing:3.0",
 	BusinessID:  ProfileIDPeppolBilling,
+	Addons:      []cbc.Key{en16931.V2017},
 }
 
 // ContextFacturX is used for Factur-X documents.
 var ContextFacturX = Context{
 	GuidelineID: "urn:cen.eu:en16931:2017#conformant#urn:factur-x.eu:1p0:extended",
+	Addons:      []cbc.Key{facturx.V1},
 }
 
 // ContextZUGFeRD is the context used for ZUGFeRD documents which is identical to
 // FacturX
-var ContextZUGFeRD = ContextFacturX
+var ContextZUGFeRD = Context{
+	GuidelineID: "urn:cen.eu:en16931:2017#conformant#urn:factur-x.eu:1p0:extended",
+	Addons:      []cbc.Key{zugferd.V2},
+}
 
 // ContextXRechnung is used for XRechnung documents
 var ContextXRechnung = Context{
 	GuidelineID: "urn:cen.eu:en16931:2017#compliant#urn:xeinkauf.de:kosit:xrechnung_3.0",
 	BusinessID:  ProfileIDPeppolBilling,
+	Addons:      []cbc.Key{xrechnung.V3},
 }
 
 // ParseInvoice parses a raw XML CII Invoice and converts it into
@@ -78,10 +91,19 @@ func ConvertInvoice(env *gobl.Envelope, opts ...Option) (*Invoice, error) {
 	for _, opt := range opts {
 		opt(o)
 	}
+
 	inv, ok := env.Extract().(*bill.Invoice)
 	if !ok {
 		return nil, fmt.Errorf("expected bill/invoice, got %T", env.Extract())
 	}
+
+	// Check addons
+	for _, ao := range o.context.Addons {
+		if !ao.In(inv.GetAddons()...) {
+			return nil, fmt.Errorf("gobl invoice missing addon %s", ao)
+		}
+	}
+
 	return newInvoice(inv, o.context)
 }
 
